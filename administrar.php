2e632ede753e981944d8b9a1b1b680b224407215
alumno <alumno>
2017-04-10 20:36:31 -0300

    Clase 4 Commit 3

--- administrar.php
+++ administrar.php
@@ -1,5 +1,35 @@
 <?php
+    $destino = "upload/".$_FILES["archivoASubir"]["name"];
+    $flag=TRUE;
+    
+    var_dump($_FILES);
+    
+    $formatoImagen = $_FILES["archivoASubir"]["type"];
+    //echo '<a href="mostrarimagen.html">Mostrar</a>'
+    //echo '<img src="upload/Jellyfish.jpg" alt="Jellyfish">';
+    //echo '<img src="'.$destino.'">';
 
-var_dump($_FILES);
+    if($formatoImagen!="image/jpeg" && $formatoImagen!="image/png")
+    {
+        echo "El archivo no es una imagen";
+        $flag=FALSE;
+    }
 
+    if($_FILES["archivoASubir"]["size"]>1000000)
+    {
+        echo "Archivo demasiado grande";
+        $flag=FALSE;
+    }
+
+    if(file_exists($destino))
+    {
+        echo "El archivo ya existe";
+        copy($destino,"backup/");
+    }
+
+    if($flag=TRUE)
+    {
+       
+        move_uploaded_file($_FILES["archivoASubir"]["tmp_name"],$destino);
+    }
 ?>
